5be4af8cf813d6592b0a4428ee4d6dd58cf0ad62
devToroko <51431766+devToroko@users.noreply.github.com>
2023-04-15 14:26:28 +0900

    commit

---
+++ order/src/main/java/shop/domain/Order.java
@@ -0,0 +1,43 @@
+package shop.domain;
+
+import java.util.Date;
+import java.util.List;
+import javax.persistence.*;
+import lombok.Data;
+import shop.OrderApplication;
+import shop.domain.OrderCanceled;
+import shop.domain.OrderPlaced;
+
+@Entity
+@Table(name = "Order_table")
+@Data
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String productId;
+
+    private String customerId;
+
+    private Integer qty;
+
+    private String status;
+
+    @PostPersist
+    public void onPostPersist() {
+        OrderPlaced orderPlaced = new OrderPlaced(this);
+        orderPlaced.publishAfterCommit();
+
+        OrderCanceled orderCanceled = new OrderCanceled(this);
+        orderCanceled.publishAfterCommit();
+    }
+
+    public static OrderRepository repository() {
+        OrderRepository orderRepository = OrderApplication.applicationContext.getBean(
+            OrderRepository.class
+        );
+        return orderRepository;
+    }
+}
---
+++ order/src/main/java/shop/domain/OrderCanceled.java
@@ -0,0 +1,21 @@
+package shop.domain;
+
+import java.util.*;
+import lombok.*;
+import shop.domain.*;
+import shop.infra.AbstractEvent;
+
+@Data
+@ToString
+public class OrderCanceled extends AbstractEvent {
+
+    private Long id;
+
+    public OrderCanceled(Order aggregate) {
+        super(aggregate);
+    }
+
+    public OrderCanceled() {
+        super();
+    }
+}
